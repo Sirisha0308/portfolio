f865be31a4bbc9e74b8f689c394b0ef8831fc8cb
Sirisha0308 <bandarusirisha03@gmail.com>
2025-08-30 16:00:39 +0530

    Initial commit - Portfolio

---
+++ script.js
@@ -0,0 +1,8 @@
+
+document.addEventListener('DOMContentLoaded', function(){
+  const navToggle = document.getElementById('navToggle');
+  const navMenu = document.getElementById('navMenu');
+  if(navToggle && navMenu){
+    navToggle.addEventListener('click', ()=> navMenu.classList.toggle('open'));
+  }
+});
